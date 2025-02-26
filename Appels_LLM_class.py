9e4470359f3f4bc9fe039e40f070621d6f8b3ccd
acleul <acalvez@eulidia.com>
2025-02-26 08:41:15 +0100

    Update Appels_LLM_class.py

--- Appels_LLM_class.py
+++ Appels_LLM_class.py
@@ -254,12 +254,12 @@ class Appels_LLM:
 
         specialties = {
         "Maternités": ["Accouchements normaux", "Accouchements à risques"],
-        "Cardiologie": ["Angioplastie coronaire", "Cardiologie interventionnelle", "Chirurgie cardiaque adulte", "Chirurgie cardiaque de l’enfant et de l’adolescent", "Infarctus du myocarde", "Insuffisance cardiaque", "Rythmologie"],
+        "Cardiologie": ["Angioplastie coronaire", "Cardiologie interventionnelle", "Chirurgie cardiaque adulte", "Chirurgie cardiaque pédiatrique", "Infarctus du myocarde", "Insuffisance cardiaque", "Rythmologie"],
         "Veines et artères": ["Ablation des varices", "Chirurgie des artères", "Chirurgie des carotides", "Hypertension artérielle", "Médecine vasculaire"],
         "Orthopédie": ["Arthrose de la main", "Chirurgie de l'épaule", "Chirurgie de la cheville", "Chirurgie du canal carpien", "Chirurgie du dos de l'adulte", "Chirurgie du dos de l'enfant et de l’adolescent", "Chirurgie du pied", "Ligaments du genou", "Prothèse de genou", "Prothèse de hanche"],
-        "Ophtalmologie": ["Cataracte", "Chirurgie de la cornée", "Chirurgie de la rétine", "Glaucome", "Strabisme"],
+        "Ophtalmologie": ["Cataracte", "Chirurgie de la cornée", "Rétine", "Glaucome", "Strabisme"],
         "Gynécologie et cancers de la femme": ["Cancer de l'ovaire", "Cancer de l'utérus", "Cancer du sein", "Endométriose", "Fibrome utérin"],
-        "Appareil digestif": ["Appendicite", "Cancer de l'estomac ou de l'œsophage", "Cancer du côlon ou de l'intestin", "Cancer du foie", "Cancer du pancréas", "Chirurgie de l'obésité", "Chirurgie du rectum", "Hernies de l'abdomen", "Maladies inflammatoires chroniques de l'intestin (MICI)", "Proctologie"],
+        "Appareil digestif": ["Appendicite", "Cancer de l'estomac et de l'œsophage", "Cancer du colon ou de l'intestin", "Cancer du foie", "Cancer du pancréas", "Chirurgie de l'obésité", "Chirurgie du rectum", "Hernies de l'abdomen", "Maladies inflammatoires chroniques de l'intestin (MICI)", "Proctologie"],
         "Psychiatrie": ["Dépression", "Schizophrénie"],
         "Urologie": ["Adénome de la prostate", "Calculs urinaires", "Cancer de la prostate", "Cancer de la vessie", "Cancer du rein", "Chirurgie des testicules de l’adulte", "Chirurgie des testicules de l’enfant et de l’adolescent"],
         "Tête et cou": ["Amygdales et végétations", "Audition", "Cancer ORL", "Chirurgie dentaire et orale de l’adulte", "Chirurgie dentaire et orale de l’enfant et de l’adolescent", "Chirurgie du nez et des sinus", "Chirurgie maxillo-faciale", "Glandes salivaires"],
